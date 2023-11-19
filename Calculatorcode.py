1c4d45f3a236d218b68f66c4fbf85993b2d94ae2
Abel JS <110282597+AJS2k20@users.noreply.github.com>
2023-11-19 14:43:04 +0530

    Update Calculatorcode.py

--- Calculatorcode.py
+++ Calculatorcode.py
@@ -1,10 +1,5 @@
 def calculator():
-    """
-    A simple calculator with basic arithmetic operations.
 
-    Prompt the user to input two numbers and an operation choice.
-    Perform the calculation and display the result.
-    """
     num1 = float(input("Enter the first number: "))
     num2 = float(input("Enter the second number: "))
     operation = input("Choose an operation (+, -, *, /): ")
@@ -23,7 +18,6 @@ def calculator():
             return
 
     print(f"The result of {num1} {operation} {num2} is {result}")
-
 
 # Call the calculator function to start the program
 calculator()
