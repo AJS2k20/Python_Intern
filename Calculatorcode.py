81d2e8842677d40072d27729436295d86bf3a81a
Abel JS <110282597+AJS2k20@users.noreply.github.com>
2023-11-19 14:33:48 +0530

    Update Calculatorcode.py

--- Calculatorcode.py
+++ Calculatorcode.py
@@ -1,45 +1,29 @@
-# Define the function for addition
-def add(x, y):
-    return x + y
+def calculator():
+    """
+    A simple calculator with basic arithmetic operations.
 
-# Define the function for subtraction
-def subtract(x, y):
-    return x - y
+    Prompt the user to input two numbers and an operation choice.
+    Perform the calculation and display the result.
+    """
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    operation = input("Choose an operation (+, -, *, /): ")
 
-# Define the function for multiplication
-def multiply(x, y):
-    return x * y
+    if operation == '+':
+        result = num1 + num2
+    elif operation == '-':
+        result = num1 - num2
+    elif operation == '*':
+        result = num1 * num2
+    elif operation == '/':
+        if num2 != 0:
+            result = num1 / num2
+        else:
+            print("Error: Division by zero")
+            return
 
-# Define the function for division
-def divide(x, y):
-    if y == 0:
-        return "Cannot divide by zero"
-    return x / y
+    print(f"The result of {num1} {operation} {num2} is {result}")
 
-# Main calculator loop
-while True:
-    print("Options:")
-    print("Enter 'add' for addition")
-    print("Enter 'subtract' for subtraction")
-    print("Enter 'multiply' for multiplication")
-    print("Enter 'divide' for division")
-    print("Enter 'quit' to end the program")
 
-    user_input = input(": ")
-    if user_input == "quit":
-        break
-    elif user_input in ("add", "subtract", "multiply", "divide"):
-        num1 = float(input("Enter first number: "))
-        num2 = float(input("Enter second number: "))
-    
-    if user_input == "add":
-        print("Result: " + str(add(num1, num2)))
-    elif user_input == "subtract":
-        print("Result: " + str(subtract(num1, num2)))
-    elif user_input == "multiply":
-        print("Result: " + str(multiply(num1, num2)))
-    elif user_input == "divide":
-        print("Result: " + str(divide(num1, num2)))
-else:
-    print("Invalid input")
-
+# Call the calculator function to start the program
+calculator()
